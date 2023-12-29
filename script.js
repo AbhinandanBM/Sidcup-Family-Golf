0eff5b0030dcefff6c14069c6ded11fc450e9ffb
AbhinandanBM <bmabhi13@gmail.com>
2023-12-29 13:02:40 +0530

    scroller done

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+let crsr = document.querySelector("#cursor");
+let blur = document.querySelector("#cursor-blur");
+document.addEventListener("mousemove", function (dets) {
+  crsr.style.left = dets.x + "px";
+  crsr.style.top = dets.y + "px";
+  blur.style.left = dets.x - 200 + "px";
+  blur.style.top = dets.y - 200 + "px";
+});
+
 gsap.to("#nav", {
   backgroundColor: "#000",
   height: "90px",
@@ -5,9 +14,21 @@ gsap.to("#nav", {
   scrollTrigger: {
     trigger: "#nav",
     scroller: "body",
-    markers: true,
+    // markers: true,
     start: "top -10%",
     end: "top -11%",
     scrub: 1,
+  },
+});
+
+gsap.to("#main", {
+  backgroundColor: "#000",
+  scrollTrigger: {
+    trigger: "#main",
+    scroller: "body",
+    // markers: true,
+    start: "top -30%",
+    end: "top -80%",
+    scrub: 2,
   },
 });
